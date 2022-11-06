eb83af53bd1dc0c3999e7b1c917546bec2c980e8
mkizka <g.mkizka@gmail.com>
2022-11-07 01:29:59 +0900

    BundleAnalyzerPlugin消す

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = [
       new CopyPlugin({
         patterns: [{ from: PUBLIC_ROOT, to: DIST_PUBLIC }],
       }),
-      new BundleAnalyzerPlugin(),
+      //new BundleAnalyzerPlugin(),
     ],
     resolve: {
       extensions: [".js", ".jsx"],
