982642ce0922c568885d625db8d09c9e06c22a38
mkizka <g.mkizka@gmail.com>
2022-11-06 22:37:25 +0900

    画像をいい感じに

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -18,26 +18,6 @@ import { ChargeDialog } from "./internal/ChargeDialog";
 import { HeroImage } from "./internal/HeroImage";
 import { RecentRaceList } from "./internal/RecentRaceList";
 
-/**
- * @param {Model.Race[]} todayRaces
- * @returns {string | null}
- */
-function useHeroImage(todayRaces) {
-  const firstRaceId = todayRaces[0]?.id;
-  const url =
-    firstRaceId !== undefined
-      ? `/api/hero?firstRaceId=${firstRaceId}`
-      : "/api/hero";
-  const { data } = useFetch(url, jsonFetcher);
-
-  if (firstRaceId === undefined || data === null) {
-    return null;
-  }
-
-  const imageUrl = `${data.url}?${data.hash}`;
-  return imageUrl;
-}
-
 /** @type {React.VFC} */
 export const Top = () => {
   const { date = moment().format("YYYY-MM-DD") } = useParams();
@@ -85,11 +65,10 @@ export const Top = () => {
             isSameDay(race.startAt, date),
           )
       : [];
-  const heroImageUrl = useHeroImage(todayRaces);
 
   return (
     <Container>
-      {heroImageUrl !== null && <HeroImage url={heroImageUrl} />}
+      {<HeroImage url={"/assets/images/hero.webp"} />}
 
       <Spacer mt={Space * 2} />
       {userData && (
