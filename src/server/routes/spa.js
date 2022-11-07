d7a87578566da7bb15ef74e5e9e58dc11f18b066
mkizka <g.mkizka@gmail.com>
2022-11-07 22:44:36 +0900

    Revert "gzip"
    This reverts commit ba5f1f09e62f1ed931288b4d44f8fa5948965a90.

--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -1,13 +1,11 @@
 import { join } from "path";
 
-import fastifyCompress from "fastify-compress";
 import fastifyStatic from "fastify-static";
 
 /**
  * @type {import('fastify').FastifyPluginCallback}
  */
 export const spaRoute = async (fastify) => {
-  fastify.register(fastifyCompress);
   fastify.register(fastifyStatic, {
     root: join(__dirname, "public"),
     wildcard: false,
